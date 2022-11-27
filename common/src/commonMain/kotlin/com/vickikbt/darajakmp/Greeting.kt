3f444bafec316d28fb83efd30e2081a8b2d76a93
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-11-27 22:01:49 +0300

    Delete unused files

--- common/src/commonMain/kotlin/com/vickikbt/darajakmp/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.vickikbt.darajakmp
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greeting(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- common/src/commonMain/kotlin/com/vickikbt/darajakmp/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.vickikbt.darajakmp
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
