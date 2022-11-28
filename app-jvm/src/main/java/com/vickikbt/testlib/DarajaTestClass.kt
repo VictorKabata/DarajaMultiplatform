81f08896b708fc3385d19fa3d6734960ae965fe6
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-11-28 19:18:46 +0300

    Rename module to app-jvm

--- app-jvm/src/main/java/com/vickikbt/testlib/DarajaTestClass.kt
+++ app-jvm/src/main/java/com/vickikbt/testlib/DarajaTestClass.kt
@@ -1,28 +1,28 @@
 package com.vickikbt.testlib
 
 import com.vickikbt.darajakmp.Daraja
-import com.vickikbt.darajakmp.network.models.DarajaToken
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 fun main() {
     Napier.base(DebugAntilog())
 
-    val daraja: Daraja = Daraja(
+    val daraja = Daraja(
         consumerKey = "NrF3UW9YCIUeTeLeamBC9HRjlaGkw6RZ",
         consumerSecret = "lARzdAdZaRAtrXZ0"
     )
 
-    var token: DarajaToken? = null
-
     CoroutineScope(Dispatchers.Default).launch {
-        token = daraja.requestAuthToken()
+        val token = daraja.requestAuthToken()
 
-        Napier.i(tag = "UI", message = "Auth token: $token")
+        println("Auth token: ${token}")
+
+        Napier.i(tag = "UI inside", message = "Auth token: ${token}")
     }
 
-    Napier.i(tag = "UI", message = "Auth token: $token")
 }
