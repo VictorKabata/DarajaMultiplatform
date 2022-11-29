23e5f3cd5bd862a8154569e5438d06e9706ec308
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-11-29 15:17:49 +0300

    Linting

--- app-jvm/src/main/java/com/vickikbt/testlib/DarajaTestClass.kt
+++ app-jvm/src/main/java/com/vickikbt/testlib/DarajaTestClass.kt
@@ -1,10 +1,8 @@
 package com.vickikbt.testlib
 
 import com.vickikbt.darajakmp.Daraja
-import com.vickikbt.darajakmp.utils.getDarajaTimestamp
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
-import kotlinx.coroutines.runBlocking
 
 fun main() {
     Napier.base(DebugAntilog())
