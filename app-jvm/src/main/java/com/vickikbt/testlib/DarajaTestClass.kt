cb53b0de57195b7b6ba6523b3b5685b4724d788c
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-12-02 18:46:42 +0300

    Update DarajaTestClass.kt

--- app-jvm/src/main/java/com/vickikbt/testlib/DarajaTestClass.kt
+++ app-jvm/src/main/java/com/vickikbt/testlib/DarajaTestClass.kt
@@ -4,10 +4,8 @@ import com.vickikbt.darajakmp.Daraja
 import com.vickikbt.darajakmp.utils.DarajaTransactionType
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
-import kotlinx.coroutines.runBlocking
 
 fun main() {
-    Napier.base(DebugAntilog())
 
     Napier.i("Starting JVM app...")
 
@@ -17,16 +15,14 @@ fun main() {
         .setPassKey("bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919")
         .build()
 
-    val mpesaStkResponse = runBlocking {
-        daraja.initiateDarajaStk(
-            businessShortCode = "174379",
-            transactionDesc = "Mpesa payment",
-            amount = 1,
-            transactionType = DarajaTransactionType.CustomerBuyGoodsOnline,
-            callbackUrl = "https://mydomain.com/path",
-            phoneNumber = "0714091304"
-        )
-    }
+    val mpesaStkResponse = daraja.initiateDarajaStk(
+        businessShortCode = "174379",
+        amount = 1,
+        phoneNumber = "0714091304",
+        transactionType = DarajaTransactionType.CustomerBuyGoodsOnline,
+        transactionDesc = "Mpesa payment",
+        callbackUrl = "https://mydomain.com/path"
+    )
 
     Napier.i("Mpesa STK response: $mpesaStkResponse")
 
