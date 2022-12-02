ffb494d523893b2234e315010c1575857e752908
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-12-02 17:16:37 +0300

    Setup daraja builder

--- daraja-common/src/commonMain/kotlin/com/vickikbt/darajakmp/Daraja.kt
+++ daraja-common/src/commonMain/kotlin/com/vickikbt/darajakmp/Daraja.kt
@@ -5,22 +5,55 @@ import com.vickikbt.darajakmp.network.DarajaHttpClient
 import com.vickikbt.darajakmp.network.models.DarajaPaymentRequest
 import com.vickikbt.darajakmp.network.models.DarajaToken
 import com.vickikbt.darajakmp.utils.DarajaEnvironment
+import com.vickikbt.darajakmp.utils.DarajaTransactionType
 import com.vickikbt.darajakmp.utils.getDarajaPassword
+import com.vickikbt.darajakmp.utils.getDarajaPhoneNumber
 import com.vickikbt.darajakmp.utils.getDarajaTimestamp
 import io.ktor.client.HttpClient
+import kotlinx.datetime.Clock
 
-class Daraja(
-    private val consumerKey: String,
-    private val consumerSecret: String,
-    private val environment: DarajaEnvironment = DarajaEnvironment.SANDBOX_ENVIRONMENT
+class Daraja private constructor(
+    val consumerKey: String?,
+    val consumerSecret: String?,
+    val passKey: String?,
+    val environment: DarajaEnvironment?
 ) {
 
-    private val darajaHttpClient: HttpClient =
-        DarajaHttpClient(environment = environment).createDarajaHttpClient()
+    data class Builder(
+        var consumerKey: String? = null,
+        var consumerSecret: String? = null,
+        var passKey: String? = null,
+        var environment: DarajaEnvironment? = null
+    ) {
+        fun setConsumerKey(consumerKey: String) = apply { this.consumerKey = consumerKey }
+
+        fun setConsumerSecret(consumerSecret: String) =
+            apply { this.consumerSecret = consumerSecret }
+
+        fun setPassKey(passKey: String) = apply { this.passKey = passKey }
+
+        fun isSandbox(environment: DarajaEnvironment = DarajaEnvironment.SANDBOX_ENVIRONMENT) =
+            apply { this.environment = environment }
+
+        fun isProduction(environment: DarajaEnvironment = DarajaEnvironment.PRODUCTION_ENVIRONMENT) =
+            apply { this.environment = environment }
+
+        fun build(): Daraja = Daraja(
+            consumerKey = consumerKey,
+            consumerSecret = consumerSecret,
+            passKey = passKey,
+            environment = environment
+        )
+    }
+
+    private val darajaHttpClient: HttpClient = DarajaHttpClient(
+        environment = environment ?: DarajaEnvironment.SANDBOX_ENVIRONMENT
+    ).createDarajaHttpClient()
+
     private val darajaApiService: DarajaApiService = DarajaApiService(
         httpClient = darajaHttpClient,
-        consumerKey = consumerKey,
-        consumerSecret = consumerSecret
+        consumerKey = consumerKey ?: "",
+        consumerSecret = consumerSecret ?: ""
     )
 
     // ToDo: Set as internal
@@ -31,19 +64,18 @@ class Daraja(
 
     suspend fun initiateDarajaStk(
         businessShortCode: String,
-        passkey: String,
-        transactionDesc: String,
         amount: Int,
-        transactionType: String,
         phoneNumber: String,
+        transactionType: DarajaTransactionType,
+        transactionDesc: String,
         callbackUrl: String,
         accountReference: String? = null
     ) {
-        val timestamp = getDarajaTimestamp()
+        val timestamp = Clock.System.now().getDarajaTimestamp()
 
         val darajaPassword = getDarajaPassword(
             shortCode = businessShortCode,
-            passkey = passkey,
+            passkey = passKey ?: "",
             timestamp = timestamp
         )
 
@@ -53,13 +85,14 @@ class Daraja(
             timestamp = timestamp,
             transactionDesc = transactionDesc,
             amount = amount.toString(),
-            transactionType = transactionType, // ToDo: Create transaction type enums
-            phoneNumber = phoneNumber, // ToDo: Format phone number
+            transactionType = transactionType.name,
+            phoneNumber = phoneNumber.getDarajaPhoneNumber() ?: phoneNumber,
             callBackUrl = callbackUrl, // ToDo: Figure out how callback urls work
             accountReference = accountReference ?: businessShortCode,
             partyA = phoneNumber,
             partyB = businessShortCode
         )
+
         darajaApiService.requestMpesaStk(darajaPaymentRequest = darajaPaymentRequest)
     }
 
