976db669efa5f5345a1f56cfd541ff8aa7e3d8d1
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-12-02 17:16:19 +0300

    Setup utils

--- daraja-common/src/commonMain/kotlin/com/vickikbt/darajakmp/utils/Utils.kt
+++ daraja-common/src/commonMain/kotlin/com/vickikbt/darajakmp/utils/Utils.kt
@@ -1,34 +1,47 @@
 package com.vickikbt.darajakmp.utils
 
-import io.github.aakira.napier.Napier
 import io.ktor.util.InternalAPI
 import io.ktor.util.encodeBase64
-import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 
 /*Format current date-time to YYYYMMDDHHmmss format*/
-internal fun getDarajaTimestamp(): String {
-    val currentDateTime = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+internal fun Instant.getDarajaTimestamp(): String {
+    val currentDateTime = this.toLocalDateTime(TimeZone.currentSystemDefault())
 
     val year = currentDateTime.year
-    val month = currentDateTime.monthNumber
-    val dayOfMonth = currentDateTime.dayOfMonth
-    val hour = currentDateTime.hour
-    val minutes = currentDateTime.minute
-    val seconds = currentDateTime.second
+    val month =
+        if (currentDateTime.monthNumber < 10) "0${currentDateTime.monthNumber}" else currentDateTime.monthNumber
+    val dayOfMonth =
+        if (currentDateTime.dayOfMonth < 10) "0${currentDateTime.dayOfMonth}" else currentDateTime.dayOfMonth
+    val hour = if (currentDateTime.hour < 10) "0${currentDateTime.hour}" else currentDateTime.hour
+    val minutes =
+        if (currentDateTime.minute < 10) "0${currentDateTime.minute}" else currentDateTime.minute
+    val seconds =
+        if (currentDateTime.second < 10) "0${currentDateTime.second}" else currentDateTime.second
 
     val timestamp = "$year$month$dayOfMonth$hour$minutes$seconds"
+
     return timestamp
 }
 
 // Shortcode+Passkey+Timestamp
 @OptIn(InternalAPI::class)
-fun getDarajaPassword(shortCode: String, passkey: String, timestamp: String): String {
+internal fun getDarajaPassword(shortCode: String, passkey: String, timestamp: String): String {
     val password = shortCode + passkey + timestamp
     val darajaPassword = password.encodeBase64()
 
-    Napier.i("Daraja password: $darajaPassword")
-
     return darajaPassword
+}
+
+internal fun String.getDarajaPhoneNumber(): String? {
+    if (this.isBlank()) return null
+    if (this.length < 11 && this.startsWith("0")) {
+        return this.replaceFirst("^0".toRegex(), "254")
+    }
+    return if (this.length == 13 && this.startsWith("+")) this.replaceFirst(
+        "^+".toRegex(),
+        ""
+    ) else this
 }
