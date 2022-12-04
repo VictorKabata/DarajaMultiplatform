412ff781b35c076339a70bf5ecb2edae9ab7cb61
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-12-04 21:19:45 +0300

    Create DarajaApiServiceTest.kt

---
+++ daraja/src/commonTest/kotlin/com/vickikbt/darajakmp/network/DarajaApiServiceTest.kt
@@ -0,0 +1,53 @@
+package com.vickikbt.darajakmp.network
+
+import com.vickikbt.darajakmp.network.models.DarajaToken
+import io.ktor.client.HttpClient
+import io.mockk.coEvery
+import io.mockk.mockk
+import io.mockk.unmockkAll
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+import kotlinx.coroutines.test.runTest
+
+class DarajaApiServiceTest {
+
+    private lateinit var mockHttpClient: HttpClient
+    private lateinit var darajaApiService: DarajaApiService
+
+    private val authToken: DarajaToken =
+        DarajaToken(accessToken = "access_token", expiresIn = "expires_in")
+
+    @BeforeTest
+    fun setup() {
+        mockHttpClient = mockk(relaxed = true)
+
+        darajaApiService =
+            DarajaApiService(
+                httpClient = mockHttpClient,
+                consumerKey = "consumer_key",
+                consumerSecret = "consumer_secret"
+            )
+    }
+
+    @AfterTest
+    fun teardown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun getAuthToken_returns_result_success_on_success() = runTest {
+        // given
+        coEvery { darajaApiService.getAuthToken() } returns Result.success(authToken)
+
+        //when
+        val result = darajaApiService.getAuthToken()
+
+        //then
+        assertTrue(actual = result.isSuccess)
+        assertEquals(expected = Result.success(authToken), actual = result)
+    }
+
+}
