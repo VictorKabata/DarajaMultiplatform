74fe67b84c4b33210eed2f146ee23f9a9db83305
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-12-05 13:57:32 +0300

    Update DarajaSafeApiCall.kt

--- daraja/src/commonMain/kotlin/com/vickikbt/darajakmp/network/DarajaSafeApiCall.kt
+++ daraja/src/commonMain/kotlin/com/vickikbt/darajakmp/network/DarajaSafeApiCall.kt
@@ -1,25 +1,29 @@
 package com.vickikbt.darajakmp.network
 
 import com.vickikbt.darajakmp.network.models.DarajaErrorResponse
-import io.ktor.client.features.ResponseException
-import io.ktor.util.network.UnresolvedAddressException
+import io.ktor.client.features.ClientRequestException
+import io.ktor.client.features.ServerResponseException
 import io.ktor.utils.io.ByteReadChannel
+import io.ktor.utils.io.errors.IOException
+import kotlinx.serialization.SerializationException
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
 suspend fun <T : Any> darajaSafeApiCall(apiCall: suspend () -> T): Result<T> {
     return try {
-        Result.success(apiCall.invoke())
-    } catch (e: ResponseException) { // Non-200 error
-        Result.failure(exception = e)
-    } catch (e: UnresolvedAddressException) {
-        Result.failure(exception = e)
-    } catch (e: Exception) {
-        Result.failure(exception = e)
-    } catch (t: Throwable) {
-        Result.failure(exception = t)
+        val response = apiCall.invoke()
+        Result.success(response)
+    } catch (e: ClientRequestException) {
+        Result.failure(e)
+    } catch (e: ServerResponseException) {
+        Result.failure(e)
+    } catch (e: IOException) {
+        Result.failure(e)
+    } catch (e: SerializationException) {
+        Result.failure(e)
     }
 }
+
 
 fun getError(responseContent: ByteReadChannel): Exception {
     val response = Json.decodeFromString<DarajaErrorResponse>(string = responseContent.toString())
