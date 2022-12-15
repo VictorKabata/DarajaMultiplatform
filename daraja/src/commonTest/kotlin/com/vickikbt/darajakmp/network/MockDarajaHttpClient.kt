b4b57d21130cb038388b43f0b46640ed4a8788b9
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-12-16 02:02:34 +0300

    Updated mock ktor http client

--- daraja/src/commonTest/kotlin/com/vickikbt/darajakmp/network/MockDarajaHttpClient.kt
+++ daraja/src/commonTest/kotlin/com/vickikbt/darajakmp/network/MockDarajaHttpClient.kt
@@ -8,11 +8,14 @@ import io.ktor.client.engine.mock.MockEngine
 import io.ktor.client.engine.mock.respond
 import io.ktor.client.plugins.addDefaultResponseValidation
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.contentType
 import io.ktor.http.headersOf
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
@@ -22,15 +25,17 @@ private val responseHeaders = headersOf(HttpHeaders.ContentType, "application/js
 val mockDarajaHttpClient = HttpClient(MockEngine) {
     engine {
         addHandler { request ->
-            when (request.url.encodedPath) {
-                DarajaEndpoints.REQUEST_ACCESS_TOKEN -> {
+            println("Request: ${request.url.encodedPathAndQuery}")
+
+            when {
+                request.url.encodedPathAndQuery.contains(DarajaEndpoints.REQUEST_ACCESS_TOKEN) -> {
                     respond(AccessTokenSuccessJSON, HttpStatusCode.OK, responseHeaders)
                 }
-                DarajaEndpoints.INITIATE_MPESA_EXPRESS -> {
+                request.url.encodedPathAndQuery.contains(DarajaEndpoints.INITIATE_MPESA_EXPRESS) -> {
                     respond(MpesaExpressSuccessJSON, HttpStatusCode.OK, responseHeaders)
                 }
                 else -> {
-                    error("Unhandled ${request.url.encodedPath}")
+                    error("Unhandled ${request.url.encodedPathAndQuery}")
                 }
             }
         }
@@ -38,6 +43,8 @@ val mockDarajaHttpClient = HttpClient(MockEngine) {
 
     expectSuccess = true
     addDefaultResponseValidation()
+
+    defaultRequest { contentType(ContentType.Application.Json) }
 
     install(ContentNegotiation) {
         json(
