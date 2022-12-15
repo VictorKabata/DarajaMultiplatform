be779c52cb2fe347c4d36fccf711d80e4e7e2ce8
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-12-15 13:44:30 +0300

    Added dokka plugin

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -7,6 +7,7 @@ object BuildPlugins {
     const val detekt = "io.gitlab.arturbosch.detekt"
     const val gradleVersionUpdates = "com.github.ben-manes.versions"
     const val spotless = "com.diffplug.spotless"
+    const val dokka = "org.jetbrains.dokka"
 }
 
 object Versions {
@@ -14,6 +15,7 @@ object Versions {
     const val detekt = "1.19.0"
     const val gradleVersionUpdate = "0.42.0"
     const val spotless = "6.2.2"
+    const val dokka = "1.7.20"
 
     const val kotlinxCoroutines = "1.6.0-native-mt"
     const val ktor = "2.1.3"
