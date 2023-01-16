d947b275add6c72e1bef5103b699f50711e3b4a4
Victor Kabata <victorbro14@gmail.com>
2023-01-16 21:30:53 +0300

    Added compose compiler support for kt 1.8.0

--- app-android/build.gradle.kts
+++ app-android/build.gradle.kts
@@ -35,6 +35,11 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+
+        freeCompilerArgs += listOf(
+            "-P",
+            "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=true"
+        )
     }
     buildFeatures {
         compose = true
