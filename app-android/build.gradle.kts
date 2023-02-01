609e34edf75d442214ccee8bd691fb503de2378c
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2023-02-01 17:34:40 +0300

    Update compose compiler version

--- app-android/build.gradle.kts
+++ app-android/build.gradle.kts
@@ -35,17 +35,13 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
-
-        freeCompilerArgs += listOf(
-            "-P",
-            "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=true"
-        )
     }
+
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.0-alpha02"
+        kotlinCompilerExtensionVersion = "1.4.0"
     }
     packagingOptions {
         resources {
