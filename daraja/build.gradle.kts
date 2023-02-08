300a562a49d26a658c10f7d07ed2c68d4698a0f1
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2023-02-08 21:58:02 +0300

    Added jvm srcSet

--- daraja/build.gradle.kts
+++ daraja/build.gradle.kts
@@ -50,7 +50,7 @@ kotlin {
         }
     }
 
-    // jvm()
+    jvm()
 
     // js()
 
@@ -89,8 +89,8 @@ kotlin {
         sourceSets["iOSMain"].dependencies {}
         sourceSets["iOSTest"].dependencies {}
 
-        // sourceSets["jvmMain"].dependencies {}
-        // sourceSets["jvmTest"].dependencies {}
+        sourceSets["jvmMain"].dependencies {}
+        sourceSets["jvmTest"].dependencies {}
 
         // sourceSets["jsMain"].dependencies {}
         // sourceSets["jsTest"].dependencies {}
@@ -109,8 +109,7 @@ android {
     }
 
     buildTypes {
-        getByName("debug") {
-        }
+        getByName("debug") {}
 
         getByName("release") {
             isMinifyEnabled = false
@@ -216,9 +215,7 @@ afterEvaluate {
             val signingKey = project.get("SIGNING_PASSWORD")
 
             useInMemoryPgpKeys(
-                signingKeyId,
-                signingKeyPassword,
-                signingKey
+                signingKeyId, signingKeyPassword, signingKey
             )
             sign(publishing.publications)
         }
