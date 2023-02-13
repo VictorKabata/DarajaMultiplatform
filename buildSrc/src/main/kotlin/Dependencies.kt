1227491b5293884aedd424f981d346008dc10645
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2023-02-13 15:17:53 +0300

    Update to kotlin serialization to v1.8.10

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -34,7 +34,7 @@ object Versions {
     const val ktor = "2.2.3"
     const val kotlinxDateTime = "0.4.0"
     const val base64Encoding = "1.2.1"
-    const val kotlinSerialization = "1.8.0"
+    const val kotlinSerialization = "1.8.10"
     const val kotlinXSerialization = "1.4.1"
     const val napier = "2.6.1"
     const val cache4k = "0.9.0"
