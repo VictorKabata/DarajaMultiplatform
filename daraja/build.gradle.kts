74c5fadf4b6093779c9d3f24787e6c6eb4415347
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2023-02-16 19:22:44 +0300

    Update build.gradle.kts

--- daraja/build.gradle.kts
+++ daraja/build.gradle.kts
@@ -225,7 +225,7 @@ afterEvaluate {
 addGithubPackagesRepository()
 
 kmmbridge {
-    // mavenPublishArtifacts()
+    mavenPublishArtifacts()
     // gitTagVersions()
     // versionPrefix.set("0.9")
     // frameworkName.set("DarajaMultiplatform")
