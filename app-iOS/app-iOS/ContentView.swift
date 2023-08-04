ab109b2a7df2e603ee9b9bb6382452509a0a3409
Victor Kabata <victorbro14@gmail.com>
2023-08-05 00:17:37 +0300

    Set up iOS sample

---
+++ app-iOS/app-iOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  app-iOS
+//
+//  Created by Victor Kabata on 05/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
