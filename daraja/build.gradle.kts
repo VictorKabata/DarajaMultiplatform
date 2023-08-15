618fc61d51091bd7997189c8d4fa8a3a6459cdb2
Victor Kabata <victorbro14@gmail.com>
2023-08-16 00:28:00 +0300

    Update build.gradle.kts

--- daraja/build.gradle.kts
+++ daraja/build.gradle.kts
@@ -157,74 +157,71 @@ kover {
     }
 }
 
-afterEvaluate {
+publishing {
 
-    publishing {
+    repositories {
+        maven {
+            name = "Sonatype"
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl
+            else releasesRepoUrl
 
-        repositories {
-            maven {
-                name = "Sonatype"
-                url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl
-                else releasesRepoUrl
-
-                credentials {
-                    username = project.get("OSSRH_USERNAME")
-                    password = project.get("OSSRH_PASSWORD")
-                }
+            credentials {
+                username = project.get("OSSRH_USERNAME")
+                password = project.get("OSSRH_PASSWORD")
             }
         }
+    }
 
-        publications.withType<MavenPublication> {
+    publications.withType<MavenPublication> {
 
-            artifact(javadocJar)
+        artifact(javadocJar)
 
-            pom {
-                groupId = project.get("POM_GROUPID")
-                artifactId = project.get("POM_ARTIFACTID")
-                version = project.get("POM_VERSION")
+        pom {
+            groupId = project.get("POM_GROUPID")
+            artifactId = project.get("POM_ARTIFACTID")
+            version = project.get("POM_VERSION")
 
-                name.set(project.get("POM_NAME"))
-                description.set(project.get("POM_DESCRIPTION"))
-                url.set(project.get("POM_URL"))
+            name.set(project.get("POM_NAME"))
+            description.set(project.get("POM_DESCRIPTION"))
+            url.set(project.get("POM_URL"))
 
-                developers {
-                    developer {
-                        id.set(project.get("POM_DEVELOPER_ID"))
-                        name.set(project.get("POM_DEVELOPER_NAME"))
-                        email.set(project.get("POM_DEVELOPER_EMAIL"))
-                    }
-                }
-
-                licenses {
-                    license {
-                        name.set(project.get("POM_LICENSE_NAME"))
-                        url.set(project.get("POM_LICENSE_URL"))
-                    }
-                }
-
-                issueManagement {
-                    system.set(project.get("POM_ISSUE_SYSTEM"))
-                    url.set(project.get("POM_ISSUE_URL"))
-                }
-
-                scm {
-                    connection.set(project.get("POM_SCM_CONNECTION"))
-                    developerConnection.set(project.get("POM_SCM_DEVELOPER_CONNECTION"))
-                    url.set(project.get("POM_SCM_URL"))
+            developers {
+                developer {
+                    id.set(project.get("POM_DEVELOPER_ID"))
+                    name.set(project.get("POM_DEVELOPER_NAME"))
+                    email.set(project.get("POM_DEVELOPER_EMAIL"))
                 }
             }
-        }
 
-        signing {
-            val signingKeyId = project.get("SIGNING_ID")
-            val signingKeyPassword = project.get("SIGNING_KEY")
-            val signingKey = project.get("SIGNING_PASSWORD")
+            licenses {
+                license {
+                    name.set(project.get("POM_LICENSE_NAME"))
+                    url.set(project.get("POM_LICENSE_URL"))
+                }
+            }
 
-            useInMemoryPgpKeys(
-                signingKeyId, signingKeyPassword, signingKey
-            )
-            sign(publishing.publications)
+            issueManagement {
+                system.set(project.get("POM_ISSUE_SYSTEM"))
+                url.set(project.get("POM_ISSUE_URL"))
+            }
+
+            scm {
+                connection.set(project.get("POM_SCM_CONNECTION"))
+                developerConnection.set(project.get("POM_SCM_DEVELOPER_CONNECTION"))
+                url.set(project.get("POM_SCM_URL"))
+            }
         }
+    }
+
+    signing {
+        val signingKeyId = project.get("SIGNING_ID")
+        val signingKeyPassword = project.get("SIGNING_KEY")
+        val signingKey = project.get("SIGNING_PASSWORD")
+
+        useInMemoryPgpKeys(
+            signingKeyId, signingKeyPassword, signingKey
+        )
+        sign(publishing.publications)
     }
 }
 
