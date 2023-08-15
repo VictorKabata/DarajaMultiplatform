3804466a0dd5e4b4a647e5ebe5d9dd05fd4cffdb
Victor Kabata <victorbro14@gmail.com>
2023-08-15 17:21:21 +0300

    Created appiOS version

---
+++ app-iOS/app-iOS/app_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_iOSApp.swift
+//  app-iOS
+//
+//  Created by Victor Kabata on 15/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct app_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
