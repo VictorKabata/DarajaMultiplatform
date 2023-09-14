c37c7a8dfaf3447d48444904a039fe7bd67a9023
Victor Kabata <victorbro14@gmail.com>
2023-09-14 23:12:30 +0300

    Create C2BResponse.kt

---
+++ daraja/src/commonMain/kotlin/com/vickbt/darajakmp/network/models/C2BResponse.kt
@@ -0,0 +1,22 @@
+package com.vickbt.darajakmp.network.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlin.native.ObjCName
+
+@ObjCName(swiftName = "C2BResponse")
+@Serializable
+/***/
+data class C2BResponse(
+    /**This is a global unique identifier for the transaction request returned by the API proxy upon successful request submission.*/
+    @SerialName("OriginatorCoversationID")
+    val originatorCoversationId: String,
+
+    /**It indicates whether Mobile Money accepts the request or not.*/
+    @SerialName("ResponseCode")
+    val responseCode: String,
+
+    /**This is the status of the request.*/
+    @SerialName("ResponseDescription")
+    val responseDescription: String
+)
