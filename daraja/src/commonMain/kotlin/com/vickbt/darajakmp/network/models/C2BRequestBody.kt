9cc9ab7bb76312b51ead4807e818ebe5adbf88c3
Victor Kabata <victorbro14@gmail.com>
2023-09-14 22:10:35 +0300

    Delete unused data class

--- daraja/src/commonMain/kotlin/com/vickbt/darajakmp/network/models/C2BRequestBody.kt
+++
@@ -1,8 +0,0 @@
-package com.vickbt.darajakmp.network.models
-
-data class C2BRequestBody(
-    val ConfirmationURL: String,
-    val ResponseType: String,
-    val ShortCode: Int,
-    val ValidationURL: String
-)
