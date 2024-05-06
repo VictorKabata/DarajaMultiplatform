85f29ebf44ae8c6e6b7bdb02ff6f3f8b2a9611c6
Victor Kabata <victorbro14@gmail.com>
2024-05-06 08:07:37 +0300

    Linting

--- daraja/src/commonMain/kotlin/com/vickbt/darajakmp/utils/DarajaRunBlocking.kt
+++ daraja/src/commonMain/kotlin/com/vickbt/darajakmp/utils/DarajaRunBlocking.kt
@@ -1,9 +1,32 @@
+/*
+ * Copyright 2024 Daraja Multiplatform
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.vickbt.darajakmp.utils
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlin.coroutines.CoroutineContext
 
+/**Util function to allow execution of run blocking code in [jsTarget]
+ *
+ * This works the same way as std Kotlin's [runBlocking]
+ *
+ * @param [context] Coroutine context where the code will be executed
+ * @param [block] Suspending code that needs to be executed in the coroutine context
+ * */
 expect fun <T> darajaRunBlocking(
     context: CoroutineContext = Dispatchers.Default,
     block: suspend CoroutineScope.() -> T
