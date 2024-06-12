9f0352fe84570f36bafe926d70ff1fa5e96190c6
Victor Kabata <victorbro14@gmail.com>
2024-06-12 12:47:31 +0300

    Fix android sample issue

--- app-android/build.gradle.kts
+++ app-android/build.gradle.kts
@@ -38,6 +38,11 @@ android {
         compose = true
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     kotlin {
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_17)
