29800adf539147ca1c2bc8974e9003b11eb7e4a7
Victor Kabata <victorbro14@gmail.com>
2024-08-17 15:01:09 +0300

    Added work around for publishing & signing tasks dependency issue

--- daraja/build.gradle.kts
+++ daraja/build.gradle.kts
@@ -160,7 +160,12 @@ publishing {
     repositories {
         maven {
             name = "sonatype"
-            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            url =
+                if (version.toString().endsWith("SNAPSHOT")) {
+                    snapshotsRepoUrl
+                } else {
+                    releasesRepoUrl
+                }
 
             credentials {
                 username = project.get("OSSRH_USERNAME")
@@ -234,4 +239,11 @@ multiplatformSwiftPackage {
 // Opt-In Experimental ObjCName in Kotlin > 1.8.0
 kotlin.sourceSets.all {
     languageSettings.optIn("kotlin.experimental.ExperimentalObjCName")
+}
+
+// Work around for tasks dependency issue: https://youtrack.jetbrains.com/issue/KT-46466
+val dependsOnTasks = mutableListOf<String>()
+tasks.withType<AbstractPublishToMaven>().configureEach {
+    dependsOnTasks.add(this.name.replace("publish", "sign").replaceAfter("Publication", ""))
+    dependsOn(dependsOnTasks)
 }
