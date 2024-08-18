2e66bc608870016bffcc61d0d187bc9201ad59d8
Victor Kabata <victorbro14@gmail.com>
2024-08-18 14:05:53 +0300

    Set up basic iOS sample

---
+++ samples/ios/Daraja iOS/Daraja_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daraja_iOSApp.swift
+//  Daraja iOS
+//
+//  Created by Victor Kabata on 18/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Daraja_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
