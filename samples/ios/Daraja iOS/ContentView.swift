5a5b1af1cf26809d6a0c667febf298627b2a63a6
Victor Kabata <victorbro14@gmail.com>
2024-08-19 09:04:12 +0300

    Linting

--- samples/ios/Daraja iOS/ContentView.swift
+++ samples/ios/Daraja iOS/ContentView.swift
@@ -10,59 +10,59 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         @State var amount: Int32 = 1
-            @FocusState var isAmountTextFieldFocused: Bool
-            
-            @State var phoneNumber: String = ""
-            @FocusState var isPhoneTextFieldFocused: Bool
-        
+        @FocusState var isAmountTextFieldFocused: Bool
+
+        @State var phoneNumber: String = ""
+        @FocusState var isPhoneTextFieldFocused: Bool
+
         List {
-                    DisclosureGroup("M-Pesa Express") {
-                        VStack {
-                            // Amount textfield
-                                        TextField("Amount", text: .init(get: {"\(amount)"}, set: {
-                                            if let newValue = Int32($0) {
-                                                amount = newValue
-                                            }
-                                        }))
-                                        .padding()
-                                        .accentColor(.green)
-                                        .background(.white)
-                                        .keyboardType(.numberPad)
-                                        .overlay(
-                                            RoundedRectangle(cornerRadius: 10)
-                                                .stroke(isAmountTextFieldFocused ? Color.green: Color.gray.opacity(0.5), lineWidth: 1)
-                                        )
-                                        .cornerRadius(10)
-                                        .shadow(color: Color.gray.opacity(0.4), radius: 4, x: 0, y: 2)
-                                        .focused($isAmountTextFieldFocused)
-                                        
-                                        // Phone Number textfield
-                                        TextField("Phone Number", text: $phoneNumber)
-                                            .padding()
-                                            .accentColor(.green)
-                                            .background(.white)
-                                            .keyboardType(.phonePad)
-                                            .overlay(
-                                                RoundedRectangle(cornerRadius: 10)
-                                                    .stroke(isPhoneTextFieldFocused ? Color.green:Color.gray.opacity(0.5), lineWidth: 1)
-                                            )
-                                            .cornerRadius(10)
-                                            .shadow(color: Color.gray.opacity(0.4), radius: 4, x: 0, y: 2)
-                                            .focused($isPhoneTextFieldFocused)
-                                        
-                                        // Pay Button
-                                        Button(action: {                              }) {
-                                            Image(systemName: "plus")
-                                                .padding(20)
-                                                .background(Color.green)
-                                                .foregroundColor(.white)
-                                                .clipShape(Circle())
-                                        }
-                                    }.padding(.horizontal,24)
+            DisclosureGroup("M-Pesa Express") {
+                VStack {
+                    // Amount textfield
+                    TextField("Amount", text: .init(get: { "\(amount)" }, set: {
+                        if let newValue = Int32($0) {
+                            amount = newValue
                         }
+                    }))
+                    .padding()
+                    .accentColor(.green)
+                    .background(.white)
+                    .keyboardType(.numberPad)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(isAmountTextFieldFocused ? Color.green : Color.gray.opacity(0.5), lineWidth: 1)
+                    )
+                    .cornerRadius(10)
+                    .shadow(color: Color.gray.opacity(0.4), radius: 4, x: 0, y: 2)
+                    .focused($isAmountTextFieldFocused)
+
+                    // Phone Number textfield
+                    TextField("Phone Number", text: $phoneNumber)
+                        .padding()
+                        .accentColor(.green)
+                        .background(.white)
+                        .keyboardType(.phonePad)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(isPhoneTextFieldFocused ? Color.green : Color.gray.opacity(0.5), lineWidth: 1)
+                        )
+                        .cornerRadius(10)
+                        .shadow(color: Color.gray.opacity(0.4), radius: 4, x: 0, y: 2)
+                        .focused($isPhoneTextFieldFocused)
+
+                    // Pay Button
+                    Button(action: {}) {
+                        Image(systemName: "plus")
+                            .padding(20)
+                            .background(Color.green)
+                            .foregroundColor(.white)
+                            .clipShape(Circle())
                     }
                 }
+            }
+        }
     }
+}
 
 #Preview {
     ContentView()
