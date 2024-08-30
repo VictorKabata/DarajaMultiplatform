5fa47937856d269376eab1322f107be3a20a373b
Victor Kabata <victorbro14@gmail.com>
2024-08-30 15:39:48 +0300

    Added koin bom and koin android

--- samples/android/app/build.gradle.kts
+++ samples/android/app/build.gradle.kts
@@ -59,11 +59,17 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    implementation(platform(libs.koin.bom))
+    implementation(libs.koin.android)
+    
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
